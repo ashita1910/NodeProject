af81abd4fbf3cadd7c15699cd68bb20305ae764e
ashitag64 <ashita.goyal_cs18@gla.ac.in>
2021-04-24 20:33:39 +0530

    router for promotions and leaders added

--- node-express/routes/dishRouter.js
+++ node-express/routes/dishRouter.js
@@ -24,5 +24,26 @@ dishRouter.route('/')
 .delete((req, res, next) => {
     res.end('Deleting all dishes');
 });
+dishRouter.route('/:dishId')
+.all((req,res,next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get('/:dishId', (req,res,next) => {
+      res.end('Will send details of the dish: ' + req.params.dishId +' to you!');
+})
+.post('/:dishId', (req, res, next) => {
+    res.statusCode = 403;
+    res.end('POST operation not supported on /dishes/'+ req.params.dishId);
+})
+.put('/:dishId', (req, res, next) => {
+    res.write('Updating the dish: ' + req.params.dishId + '\n');
+    res.end('Will update the dish: ' + req.body.name + 
+          ' with details: ' + req.body.description);
+})
+.delete('/:dishId', (req, res, next) => {
+      res.end('Deleting dish: ' + req.params.dishId);
+});
 
 module.exports = dishRouter;
