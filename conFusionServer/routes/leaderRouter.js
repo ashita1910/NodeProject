a1cc1dfae41b96e91ff5c6b3faf17c0dff3759f4
ashitag64 <ashita.goyal_cs18@gla.ac.in>
2021-05-13 17:49:28 +0530

    updated

--- conFusionServer/routes/leaderRouter.js
+++ conFusionServer/routes/leaderRouter.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+var authenticate = require('../authenticate');
 
 const Leaders = require('../models/leaders');
 
