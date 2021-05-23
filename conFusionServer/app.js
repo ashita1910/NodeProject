ef6087c9d5f6a7442bb5b545a75fba7eba5c469c
ashitag64 <ashita.goyal_cs18@gla.ac.in>
2021-05-23 21:22:45 +0530

    favourites added

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -14,7 +14,8 @@ var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
-const uploadRouter = require('./routes/uploadRouter');
+var uploadRouter = require('./routes/uploadRouter');
+var favouriteRouter = require('./routes/favorites');
 
 var app = express();
 
@@ -142,6 +143,7 @@ app.use('/dishes',dishRouter);
 app.use('/promotions',promoRouter);
 app.use('/leaders',leaderRouter);
 app.use('/imageUpload',uploadRouter);
+app.use('/favorites', favouriteRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
